19ab90a9d56f3b73c318484c997f6e5a637a36fe
Sumesh Harale <harale.s@husky.neu.edu>
2020-02-17 15:00:56 -0500

    Created panels and classes

---
+++ IllegalSkillsException/src/main/java/Business/CustomerDirectory.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sumesh
+ */
+public class CustomerDirectory {
+    private ArrayList<Customer>customerList;
+
+    public ArrayList<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(ArrayList<Customer> customerList) {
+        this.customerList = customerList;
+    }
+}
---
+++ IllegalSkillsException/src/main/java/Business/Flight.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sumesh
+ */
+public class Flight {
+    private String airlinerName;
+    private String flightNumber;
+    private String source;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+    private double flightPrice;
+    private int totalSeats;
+
+    public String getAirlinerName() {
+        return airlinerName;
+    }
+
+    public void setAirlinerName(String airlinerName) {
+        this.airlinerName = airlinerName;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getFlightPrice() {
+        return flightPrice;
+    }
+
+    public void setFlightPrice(double flightPrice) {
+        this.flightPrice = flightPrice;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public ArrayList<Seats> getSeatList() {
+        return seatList;
+    }
+
+    public void setSeatList(ArrayList<Seats> seatList) {
+        this.seatList = seatList;
+    }
+
+    public Seats getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seats seat) {
+        this.seat = seat;
+    }
+    private int availableSeats;
+    private ArrayList<Seats> seatList;
+    private Seats seat;
+}
